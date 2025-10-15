77d521a781bdebf8d52400a8e410b317ed398842
Davide Magrin <davide@Davides-MacBook-Pro.local>
2025-10-15 10:03:48 +0200

    Remove default Svelte favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <nav>
 	<a href="{base}/" aria-current={$page.url.pathname === `${base}/` ? 'page' : undefined}>Home</a>
